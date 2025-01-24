76f2fed74456d3103083d2da47832efb3e6aed6e
ahmad-marie1612 <ahmedmerie1612@gmail.com>
2025-01-24 19:39:57 +0300

    Add files via upload

---
+++ firstbot.py
@@ -0,0 +1,34 @@
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+import telegram
+import telebot
+from telebot import types
+token="7644799463:AAFqhfTFKPdRhlBVunM9BqKIqvilHAZB8ZU"
+bot= telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,'أهلا بكم في بوت أحمد مرعي الأول')
+    id=message.chat.id
+    firstname=message.from_user.first_name
+    lastname=message.from_user.last_name
+    username=message.from_user.username
+    if username ==None:
+        username='-----'
+    else:
+        username=message.from_user.username
+    if lastname !=None:
+        fullname=f'{firstname} {lastname}'
+    else:
+        fullname=f'{firstname}'
+    bot.send_message(6182798160,
+    f'''  
+◀◀◀◀◀◀ NEW ▶▶▶▶▶▶
+⏺ Hi {firstname}
+⏺ ID : {id}
+⏺ First Name : {firstname}
+⏺ Last Name : {lastname}
+⏺ Full Name : {fullname}
+⏺ User Name : @{username}
+◀◀◀◀◀◀ END ▶▶▶▶▶▶
+        ''')
+bot.polling()
